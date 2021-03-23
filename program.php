3d462e513694ccb6830e29e285e12a3c58bbf896
Laben <labenh526@gmail.com>
2021-03-23 12:58:03 -0400

    Reworked logic to simplify the logic in the user input loop and have most of the functionality abstracted to command objects

--- program.php
+++ program.php
@@ -1,27 +1,61 @@
 <?php
 
-    function openCsv($filename) {
-           
+    interface Command {
+        public function execute($args);
+        public function argsAreValid($args);
+        public function help();
     }
 
-    function help() {
-        echo "\e[34m List of commands:\n\e[37m";
-        echo "  exit - exits the program\n";
-        echo "  help - lists all commands \n";
-        echo "\e[39m\n"; //back to default color
-    }
+    $exit = new class implements Command {
+
+        public function help() {
+            echo "\e[32m exit - exits the program. Takes no arguments. \n\e[39m";
+        }
+
+        public function execute($args) {
+            exit();
+        }
+
+        public function argsAreValid($args) {
+            return count($args) == 0;
+        }
+        
+    };
+
+    $help = new class implements Command {
+
+        public function execute($args) {
+            echo "\e[34m List of commands:\n\e[37m";
+            echo "  exit - exits the program\n";
+            echo "  help - lists all commands \n";
+            echo "\e[39m\n"; //back to default color
+        }
+
+        public function help() {
+            echo "\e[32m help - lists all commands. Takes no arguments \n\e[39m";
+        }
+
+        public function argsAreValid($args) {
+            return count($args) == 0;
+        }
+    };
+
+    $commands = array("exit" => $exit, "help" => $help);
 
     while (True) {
+        global $commands;
         //User input loop
         $input = rtrim(fgets(STDIN));
-
-        if (strcasecmp($input, "exit") == 0) {
-            //Command Exit: Exits the program
-            exit();
-        } elseif (strcasecmp($input, "help") == 0) {
-            help();
-        }
-        else {
+        $command = explode(' ', $input)[0];
+        if (in_array($command, array_keys($commands))) {
+            $args = array_slice(explode(' ', $input), 1);
+            if ($commands[$command]->argsAreValid($args))
+                $commands[$command]->execute($args);
+            else {
+                echo "\e[31m Invalid arguments\n";
+                $commands[$command]->help(); 
+            }
+        } else {
             echo "\e[31m Unrecognized Command: \"$input\" Type \"help\" for a list of commands\n\e[39m";
         }
     }
